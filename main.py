be9508eb5c66f66f70a077d7c5f0f2685e8963fc
Thomas Pequegnot <76632082+thompgt@users.noreply.github.com>
2025-06-05 20:01:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import requests
-import sys
-import os
-from dotenv import load_dotenv
-import boto3
+import streamlit as st
+from get_data import fetch_data
+from data_visualizations import model_price, volatility_analysis
 
-print("All set!")
-print(os.environ["ECHIOS_KEY"])
+stock_options= ['IDXX', 'INTU', 'MSFT', 'NEE', 'NVDA']
+
+st.title("AI-Powered Market Analytics")
+selected_stock= st.selectbox("Select a stock: ", stock_options)
+
+#fetch data
+df=fetch_data(selected_stock)
+st.write(f"Displaying data for {selected_stock}")
+
+#show visualizations
+fig=model_price(selected_stock)
+st.plotly_chart(fig)
+
+fig2=volatility_analysis(selected_stock)
+st.plotly_chart(fig2)
