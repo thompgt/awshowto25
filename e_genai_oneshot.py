732a5be6bee17a1a355dd4c8f2e44b2158dd4709
Manasa <manasavinodkumar@gmail.com>
2025-06-06 11:17:03 -0400

    remove ui error message

--- e_genai_oneshot.py
+++ e_genai_oneshot.py
@@ -3,6 +3,7 @@ import json
 from dotenv import load_dotenv
 import os
 
+# def gen_insight(price_is):
 load_dotenv()
 
 client = boto3.client(
@@ -23,38 +24,38 @@ price_is = input("Enter stock price to classify: ")
 one_shot_prompt = r"""
 You are an expert stock market analyst.
 
-Following is an example of classifying a stock based on price:
+Following is an example of classifying a stock based on volatility:
 
-based on the rule if stock price is less than 100, classify as 'Low', 
-if stock price is between 100 and 500, classify as 'Medium', 
-if stock price is greater than 500, classify as 'High'.
+based on the rule if stock beta is less than 1.0, classify as 'Low', 
+if beta is equal to 1.0, classify as 'Medium', 
+if beta is greater than 1.0, classify as 'High'.
 Your reply must be in the format of a key-value pair with 'class' as the key.
 
 Example:
-stock price: 150.25
+beta: 1.0
 {"class" : "Medium"}
 
-stock price: 45.75
+beta: 0.75
 {"class" : "Low"}
 
-stock price: 600.50
+beta: 1.5
 {"class": "High"}
 
-Now, classify the following stock price:
-stock price: """ + f"{price_is}"
+Now, classify the following stock beta:
+stock beta: """ + f"{price_is}"
 
 prompt = one_shot_prompt
 
 messages = [
     {
-      "role": "user",
-      "content": [
-          {
-              "text": prompt
-          }
-      ]
+    "role": "user",
+    "content": [
+        {
+            "text": prompt
+        }
+    ]
     }
-  ]
+]
 
 params = {"maxTokens": 300, "topP": 0.1, "temperature": 0.3}
 
@@ -73,6 +74,11 @@ print("-" * 40)
 ans = response['output']
 ans_class = json.loads(ans['message']['content'][0]['text'])
 print(
+    f"Answer:\n\
+Stock with price ${float(price_is):.2f} \
+is classified as {ans_class['class']}"
+)
+return (
     f"Answer:\n\
 Stock with price ${float(price_is):.2f} \
 is classified as {ans_class['class']}"
--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import streamlit as st
 from get_data import fetch_data
 from data_visualizations import model_price, volatility_analysis
 
-stock_options= ['IDXX', 'INTU', 'MSFT', 'NEE', 'NVDA']
+stock_options= ['','MSFT', 'INTU', 'IDXX', 'NEE', 'NVDA']
 
 st.title("AI-Powered Market Analytics")
 selected_stock= st.selectbox("Select a stock: ", stock_options)
 
 #fetch data
-df=fetch_data(selected_stock)
-st.write(f"Displaying data for {selected_stock}")
+try:
+    if(selected_stock==''):
+        st.write("Select a stock")
+    df=fetch_data(selected_stock)
+    st.write(f"Displaying data for {selected_stock}")
 
-#show visualizations
-fig=model_price(selected_stock)
-st.plotly_chart(fig)
+    #show visualizations
+    fig=model_price(selected_stock)
+    st.plotly_chart(fig)
 
-fig2=volatility_analysis(selected_stock)
-st.plotly_chart(fig2)
+    fig2=volatility_analysis(selected_stock)
+    st.plotly_chart(fig2)
+except:
+    if(selected_stock!=''):
+        st.write(f"Data for {selected_stock} not available")
+
+
